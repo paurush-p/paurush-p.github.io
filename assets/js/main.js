757221f2afc06ded961f7ab5fb4c7f7bb1f7a1e4
Paurush Punyasheel <57411942+paurush-p@users.noreply.github.com>
2024-12-04 23:23:20 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -189,4 +189,13 @@
 				}
 			});
 
-})(jQuery);
+}
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+)(jQuery);
